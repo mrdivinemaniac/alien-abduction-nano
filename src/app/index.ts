b99ccb82b205ef2f2753fad03911d9bfbafc8421
mrdivinemaniac <divinemaniac@gmail.com>
2021-03-06 21:41:43 +0545

    Spawn sheeps in random intervals

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,11 @@
-import { startEngine } from './engine'
+import { randBetween } from '../utils'
+import { initializeEngine } from './engine'
 
-const engine = startEngine()
+const engine = initializeEngine()
+engine.loadAssets().then(() => {
+  engine.start()
+  engine.spawnTheSheep(randBetween(1, 4))
+  setInterval(() => {
+    engine.spawnTheSheep(randBetween(1, 4))
+  }, 100)
+})
